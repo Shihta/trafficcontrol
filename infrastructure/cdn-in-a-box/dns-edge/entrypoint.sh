28e7748c2b2a8d33bcffbb1c05bb1121e1ebca89
Shihta Kuan <shihta.kuan@qct.io>
2019-03-12 13:32:19 +0800

    Add Dockerfile for dns-edge

--- infrastructure/cdn-in-a-box/dns-edge/entrypoint.sh
+++ infrastructure/cdn-in-a-box/dns-edge/entrypoint.sh
@@ -21,26 +21,6 @@ set -x
 
 BIND_DATA_DIR=${DATA_DIR}/bind
 
-create_bind_data_dir() {
-  mkdir -p ${BIND_DATA_DIR}
-
-  # populate default bind configuration if it does not exist
-  if [ ! -d ${BIND_DATA_DIR}/etc ]; then
-    mv /etc/bind ${BIND_DATA_DIR}/etc
-  fi
-  rm -rf /etc/bind
-  ln -sf ${BIND_DATA_DIR}/etc /etc/bind
-  chmod -R 0775 ${BIND_DATA_DIR}
-  chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
-
-  if [ ! -d ${BIND_DATA_DIR}/lib ]; then
-    mkdir -p ${BIND_DATA_DIR}/lib
-    chown ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}/lib
-  fi
-  rm -rf /var/lib/bind
-  ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
-}
-
 create_pid_dir() {
   mkdir -m 0775 -p /var/run/named
   chown root:${BIND_USER} /var/run/named
@@ -52,7 +32,6 @@ create_bind_cache_dir() {
 }
 
 create_pid_dir
-create_bind_data_dir
 create_bind_cache_dir
 
 cp /zone.ciab.test.template /etc/bind/zone.ciab.test
@@ -84,7 +63,7 @@ fi
 # default behaviour is to launch named
 if [[ -z ${1} ]]; then
   echo "Starting named..."
-  exec $(which named) -u ${BIND_USER} -g ${EXTRA_ARGS} 
+  exec $(which named) -u ${BIND_USER} -g ${EXTRA_ARGS}
 else
   exec "$@"
 fi
